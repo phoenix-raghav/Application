12253f632ae68e47dd13bc7755794cfce15994c3
Raghav Agarwal <raghav10022001@gmail.com>
2023-12-13 18:07:39 +0530

    Variables

--- backend/middlewares/Token.js
+++ backend/middlewares/Token.js
@@ -1,5 +1,8 @@
 import jwt from 'jsonwebtoken'; 
-const JWT_SECRET = 'Hello$World';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const verifyToken = (req,res,next) =>{
     try{
